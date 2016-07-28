5b1754f4953752ea36ff44f4797efc7c12701be8
Chen Wei Ting <wtichen.tw@gmail.com>
2016-07-28 15:42:00 +0800

    complete most parts

---
+++ main.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+filename='xiao-chi.txt'
+
+exec < $filename
+
+while read line 
+do
+    read -r -a array <<< $line
+    if [ "$1" == "get-link" ]; then
+        echo "Get links for ${array[0]} - ${array[1]}"
+        python3 link-parser.py ${array[0]} 50 ${array[1]} 
+    elif [ "$1" == "get-image" ]; then
+        echo "Download images for ${array[0]} - ${array[1]}"
+        bash curl.sh ${array[1]}
+    else 
+        echo "Get image or link"
+    fi
+done
