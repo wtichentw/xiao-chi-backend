35dce18b829088b8312c7631f4055d4d7f42c717
Chen Wei Ting <wtichen.tw@gmail.com>
2016-07-29 09:35:57 +0800

    change back

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ do
     read -r -a array <<< $line
     if [ "$1" == "get-link" ]; then
         echo "Get links for ${array[0]} - ${array[1]}"
-        echo "python3 link-parser.py ${array[0]} 450 ${array[1]} "
+        python3 link-parser.py ${array[0]} 100 ${array[1]}
     elif [ "$1" == "get-image" ]; then
         echo "Download images for ${array[0]} - ${array[1]}"
         bash curl.sh ${array[1]}
