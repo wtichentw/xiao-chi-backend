c8bf06fd11469316f820bb97c2f0e0414b986be7
Chen Wei Ting <wtichen.tw@gmail.com>
2016-08-01 15:01:30 +0800

    complete backend v1, need refine

--- upload-image.py
+++ upload-image.py
@@ -2,28 +2,54 @@ from os import curdir
 from os.path import join as pjoin
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import shutil, cgi
 
 class StoreHandler(BaseHTTPRequestHandler):
     image_path = pjoin(curdir, 'pred.jpg')
 
-    def do_GET(self):
-        if self.path == '/pred.jpg':
-            with open(self.store_path) as fh:
-                self.send_response(200)
-                self.send_header('Content-type', 'text/json')
-                self.end_headers()
-                self.wfile.write(fh.read().encode())
-
     def do_POST(self):
-        if self.path == '/pred.jpg':
-            length = self.headers['content-length']
-            data = self.rfile.read(int(length))
+        if self.path == '/pred':
+            content_type = self.headers['content-type']
+            if not content_type:
+                return (False, "Content-Type header doesn't contain boundary")
+            boundary = content_type.split("=")[1].encode()
 
-            with open(self.store_path, 'w') as fh:
-                fh.write(data.decode())
+            remainbytes = int(self.headers['content-length'])
 
-            self.send_response(200)
+            line = self.rfile.readline()
+            remainbytes -= len(line)
+            if not boundary in line:
+                return (False, "Content NOT begin with boundary")
+            line = self.rfile.readline()
+            remainbytes -= len(line)
+
+            line = self.rfile.readline()
+            remainbytes -= len(line)
+            line = self.rfile.readline()
+            remainbytes -= len(line)
+            try:
+                out = open(self.image_path, 'wb')
+            except IOError:
+                return (False, "Can't create file to write, do you have permission to write?")
+                    
+            preline = self.rfile.readline()
+            remainbytes -= len(preline)
+            while remainbytes > 0:
+                line = self.rfile.readline()
+                remainbytes -= len(line)
+                if boundary in line:
+                    preline = preline[0:-1]
+                    if preline.endswith(b'\r'):
+                        preline = preline[0:-1]
+                    out.write(preline)
+                    out.close()
+                    return (True, "File '%s' upload success!" % self.image_path)
+                else:
+                    out.write(preline)
+                    preline = line
+            return (False, "Unexpect Ends of data.")
 
 
+print ('Start Server at port', 8080)
 server = HTTPServer(('', 8080), StoreHandler)
 server.serve_forever()
