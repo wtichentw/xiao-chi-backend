ac198496ee1eee208422c0deb8de6b57848adde2
Chen Wei Ting <wtichen.tw@gmail.com>
2016-08-01 21:29:13 +0800

    complete upload.py

--- upload.py
+++ upload.py
@@ -1,53 +1,57 @@
+import os
 from prediction import *
-from os import curdir
-from os.path import join as pjoin
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class StoreHandler(BaseHTTPRequestHandler):
-    image_path = pjoin(curdir, 'pred.jpg')
-
     def do_POST(self):
         if self.path == '/pred':
+            # Set image path
+            image_path = os.path.join(os.curdir, 'pred.jpg')
+
+            # Check content type valid
             content_type = self.headers['content-type']
             if not content_type:
                 return (False, "Content-Type header doesn't contain boundary")
+
+            # Get boundary
             boundary = content_type.split("=")[1].encode()
 
+            # Get length
             remainbytes = int(self.headers['content-length'])
 
+            # Get 1st line, shoule be boundary
             line = self.rfile.readline()
             remainbytes -= len(line)
             if not boundary in line:
                 return (False, "Content NOT begin with boundary")
+
+            # Get 2nd line, shoule be content-disposition
             line = self.rfile.readline()
             remainbytes -= len(line)
 
+            # Get 3rd line, shoule be content-type
             line = self.rfile.readline()
             remainbytes -= len(line)
+
+            # Get 4th line, shoule be /r/n
             line = self.rfile.readline()
             remainbytes -= len(line)
+
+            # Open file
             try:
-                out = open(self.image_path, 'wb')
+                out = open(image_path, 'wb')
             except IOError:
                 return (False, "Can't create file to write, do you have permission to write?")
                     
-            preline = self.rfile.readline()
-            remainbytes -= len(preline)
+            # Save to file 
+            # May judge by boundary or remain byte
             while remainbytes > 0:
                 line = self.rfile.readline()
                 remainbytes -= len(line)
-                if boundary in line:
-                    preline = preline[0:-1]
-                    if preline.endswith(b'\r'):
-                        preline = preline[0:-1]
-                    out.write(preline)
-                    out.close()
-                else:
-                    out.write(preline)
-                    preline = line
+                out.write(line)
+            out.close()
 
-            print (run_inference_on_image(self.image_path))
-
+            print (run_inference_on_image(image_path))
 
 if __name__ == '__main__':
     server = HTTPServer(('', 8080), StoreHandler)
